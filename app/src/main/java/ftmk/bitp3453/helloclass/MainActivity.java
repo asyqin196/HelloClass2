e9bd7c03606d9e43b2e17f79277b46a959a41685
asyqin196 <asyqinhariss@gmail.com>
2022-11-09 23:10:49 +0800

    Lab 4 dan 2

--- app/src/main/java/ftmk/bitp3453/helloclass/MainActivity.java
+++ app/src/main/java/ftmk/bitp3453/helloclass/MainActivity.java
@@ -3,12 +3,22 @@ package ftmk.bitp3453.helloclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtVw1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtVw1 = findViewById(R.id.textView);
+    }
+
+    public void fnChangeText(View view) {
+
+        txtVw1.setText("YES! the text changed");
     }
 }
